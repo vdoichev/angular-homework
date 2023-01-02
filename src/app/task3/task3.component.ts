2472bfba9663ad58ba1a5b79af1b820bc07521a0
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2023-01-02 09:33:28 +0200

    Lesson 3 task 2

--- src/app/task3/task3.component.ts
+++ src/app/task3/task3.component.ts
@@ -8,7 +8,25 @@ import { Component } from '@angular/core';
 export class Task3Component {
   constructor() {}
 
-  Image1: string = '../../../assets/1.jpeg';
+  Image1: string = 'assets/1.jpeg';
   Image1Width: number = 500;
-  Image1Height: number  = 300;
+  Image1Height: number = 300;
+
+  index: number = 0;
+
+  ImagesArray: string[] = ['assets/1.jpeg', 'assets/2.jpeg', 'assets/3.jpeg'];
+
+  nextImage() {
+    this.index++;
+    if (this.index > this.ImagesArray.length-1) {
+      this.index = 0;
+    }
+  }
+
+  prevImage() {
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.ImagesArray.length-1;
+    }
+  }
 }
