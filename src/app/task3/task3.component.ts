0eada78ecd10b74a56736151d0ff63cac741d3f2
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2023-01-02 09:13:35 +0200

    Lesson 3 task1

--- src/app/task3/task3.component.ts
+++ src/app/task3/task3.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-task3',
   templateUrl: './task3.component.html',
-  styleUrls: ['./task3.component.css']
+  styleUrls: ['./task3.component.css'],
 })
 export class Task3Component {
+  constructor() {}
 
+  Image1: string = '../../../assets/1.jpeg';
+  Image1Width: number = 500;
+  Image1Height: number  = 300;
 }
